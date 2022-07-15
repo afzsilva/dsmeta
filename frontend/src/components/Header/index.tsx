dfb81c69a9001ed5da9d77d087e5c2361f8852d8
afzsilva <afzsilva@gmail.com>
2022-07-15 20:47:54 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import icon from '../../assets/img/image 1.svg';
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header>
+        <div className='dsmeta-logo-container'>
+            <img src={icon} alt="header"/>
+            <h1>DSMeta</h1>
+            <p>Desenvolvido por <a href="https://www.instagram.com/devsuperior.ig/">@devsuperior.ig</a></p>
+        </div>
+    </header>
+  )
+}
